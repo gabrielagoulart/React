8c65d9c0fabecfaa63ecf93b24b527a5f647d5a8
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2023-07-17 17:55:04 -0300

    useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 
-
+/* 
+========= useState =========
 import { useState } from 'react'
 
 const App = () => {
@@ -23,3 +24,56 @@ const App = () => {
 }
 
 export default App;
+*/
+
+
+import { useState, useEffect } from 'react';
+ 
+const App = () => {
+  const [items, setItems] = useState([])
+  const [resourceType, setResourceType] = useState('posts')
+
+   useEffect (() => {
+    const fetchResourceTypes = async () => {
+
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/${resourceType}`
+      );
+      
+      const responseJSON = await response.json();
+
+        setItems(responseJSON)  
+    }
+
+    fetchResourceTypes();
+   }, [resourceType])
+
+   
+
+  const changeResourceType = (resourceType) => {
+    setResourceType(resourceType)
+  }
+
+  return (
+    <div>
+      <h1>{resourceType}</h1>
+      <div style={{ display: "flex", alignItems: "center"}}>
+
+      <button onClick={() => changeResourceType("posts")}>Posts</button>
+
+      <button onClick={() => changeResourceType("comments")}>Comments</button>
+
+      <button onClick={() => changeResourceType("todos")}>Todos</button>
+
+      </div>
+
+      {items.map((item) => (
+        <p>{item.id}</p>
+      ))}
+    </div>
+  )
+}
+
+export default App;
+
+
